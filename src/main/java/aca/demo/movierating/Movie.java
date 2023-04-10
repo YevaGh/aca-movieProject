90da8dd048d3f466b5b9e6a3543c5c49fff45ee0
Yeva <khazaryaneva@gmail.com>
2023-04-10 15:27:24 +0400

    Added Movie class

---
+++ src/main/java/aca/demo/movierating/Movie.java
@@ -0,0 +1,21 @@
+package aca.demo.movierating;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Slf4j
+public class Movie {
+    private String title;
+    private Genre genre;
+
+    Movie(CreateMovie createMovie) {
+        log.debug("Constructing movie with createMovie - {}",createMovie);
+        this.title= createMovie.getTitle();
+        this.genre=createMovie.getGenre();
+    }
+}
