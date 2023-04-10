46b371627a5380d4675a147bea2795afd7935a39
Yeva <khazaryaneva@gmail.com>
2023-04-10 15:56:49 +0400

    Added MovieRepository class

---
+++ src/main/java/aca/demo/movierating/MovieRepository.java
@@ -0,0 +1,37 @@
+package aca.demo.movierating;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class MovieRepository {
+    private List<Movie> movies = new ArrayList<>();
+
+    public Optional<Movie> findByTitle(String title) {
+        log.debug("Finding movie by title");
+
+       Optional<Movie> byTitle =  movies.stream().filter(m->m.getTitle().equals(title)).findAny();
+
+       return byTitle;
+    }
+
+    public List<Movie> findByGenre(Genre genre) {
+        log.debug("Finding movies by genre");
+
+        List<Movie> byGenre =  movies.stream().filter(m->m.getGenre()==genre).collect(Collectors.toList());
+
+        return byGenre;
+    }
+
+    public void save(CreateMovie createMovie) {
+        log.debug("Creating and adding movie in list");
+
+        movies.add(new Movie(createMovie));
+    }
+}
