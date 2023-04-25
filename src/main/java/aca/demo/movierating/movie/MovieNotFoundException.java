3709b980c4fce25e31bec5ea02f22fed68fe463e
Yeva <khazaryaneva@gmail.com>
2023-04-26 01:56:15 +0400

    Add new files(classes)

---
+++ src/main/java/aca/demo/movierating/movie/MovieNotFoundException.java
@@ -0,0 +1,7 @@
+package aca.demo.movierating.movie;
+
+public class MovieNotFoundException extends RuntimeException{
+    public MovieNotFoundException(String message){
+        super(message);
+    }
+}
